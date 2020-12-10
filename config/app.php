bb63ff7df86c6529c8f19be74d789b5882ade750
renkunhong <44190997+11040678444@users.noreply.github.com>
2020-12-10 09:21:13 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -133,89 +133,38 @@ return [
     'upload_path' => ROOT_PATH . 'public' . DIRECTORY_SEPARATOR . 'uploads',
     // 资源文件路径
     'static_path' => ROOT_PATH . 'public' . DIRECTORY_SEPARATOR . 'static',
-    'ddxx'=>[
-    // // 数据库类型
-     'type'     => 'mysql',
-    // // 服务器地址
-     'hostname' => '120.79.5.57',//120.79.5.57
-    // // 数据库名
-     'database' => 'ddxx',
-    // // 用户名
-     'username' => 'root', //
-    // // 密码
-     'password' => 'ddxm2019',//ddxm2019
-    // // 端口
-     'hostport' => '3306',
-    // // 数据库编码默认采用utf8
-     'charset'  => 'utf8mb4',
-    // // 数据库表前缀
-     'prefix'   => 'tf_',
-     "authcode" => 'OV4w80Ndr23wt4yW1j',
-    // //#COOKIE_PREFIX#
-     ],
-     'ddxm_erp'=>[
-        // // 数据库类型
-         'type'     => 'mysql',
-        // // 服务器地址
-//         'hostname' => '120.79.5.57',
-         'hostname' => '127.0.0.1',
-        // // 数据库名
-         'database' => 'ddxm_erp',
-        // // 用户名
-//         'username' => 'ddxm_erp', //
-         'username' => 'root', //
-        // // 密码
-//         'password' => 'WZS5Mi4SHt8WxrPd',
-         'password' => 'root',
-        // // 端口
-//         'hostport' => '3339',
-         'hostport' => '3306',
-        // // 数据库编码默认采用utf8
-         'charset'  => 'utf8mb4',
-        // // 数据库表前缀
-         'prefix'   => 'ddxm_',
-         "authcode" => 'OV4w80Ndr23wt4yW1j',
-     ],
 
      //普通商品的七牛云图片地址
      'QINIU_URL'    =>"http://picture.ddxm661.com/",
 
      //七牛云配置
     'qiniu'=>[
-        'accesskey' => 'ChbjC0NsNlFawXdmV9GXZtaoU5rfq5ZS9d919Z1n',
-        'secretkey' => 'Fnd1ud7q77V7qlLlW0uqFna24RD2B-AI_2Jrd0IH',
-        'bucket' => 'ddxm-Item',
-        'domain'=>'picture.ddxm661.com'
+        'accesskey' => 'your accesskey',
+        'secretkey' => 'your secretkey',
+        'bucket' => 'your bucket',
+        'domain'=>'your domain'
     ],
 
     //微信小程序配置
-    'appid'=>'wx867b49bff338c6c4',
-    'AppSecret'=>'bacf8e48094026198543ecb4275daec7',
-
-    //捣蛋熊猫公众号微信配置
-//    'WeChat'   =>[
-//        'appid' =>'wxb5ee49b69efc2429',
-//        'AppSecret'=>'f775096b8d02d3a34761ccef9796c8f4',
-//        'MerchantId'    =>'1486226662',
-//        'key'      =>'daodanxiongmaokkgg4945sdfjklghgh'
-//    ],
+    'appid'=>'your appid',
+    'AppSecret'=>'your AppSecret',
 
     //捣蛋熊猫经销商后台微信配置
     'WeChat'   =>[
-        'appid' =>'wx6e3b81d3c901cbf2',
-        'AppSecret'=>'faff929ed271fe64f63e95f2128065b5',
-        'MerchantId'    =>'1486226662',
-        'key'      =>'daodanxiongmaokkgg4945sdfjklghgh'
+        'appid' =>'your appid',
+        'AppSecret'=>'your AppSecret',
+        'MerchantId'    =>'your MerchantId',
+        'key'      =>'your key'
     ],
 
     //身份证验证配置
     'OCR'   =>[
-        'appkey'    =>'93nnkacawg0b0nldtkwulwfqzbk73eup',
-        'appcode'   =>'4ed1ea8856cf4d15aff1c1a6f3e55afd'
+        'appkey'    =>'your appkey',
+        'appcode'   =>'your appcode'
     ],
 
     //支付回调地址（线上与线下区分）
-    'notify_url'=>empty(APP_DEBUG) ? 'https://ddxm661.com/wxshop/Wxnotify/' : 'https://www.ddxm661.com/wxshop/Wxnotify/',
+    'notify_url'=>empty(APP_DEBUG) ? '回调地址' : '回调地址',
 
     //2月-6余额预存送活动
     'Recharge' => [
@@ -225,7 +174,4 @@ return [
         '1200'=>12000,
         'rate'  =>0.1       //购买商品时优惠的折扣
     ],
-
-    //erp系统地址
-    'erp_url'   =>'http://testadmin2.ddxm661.com/',
 ];
